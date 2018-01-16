d039ee15ab1555c2e0cec9ba474cb43cd573ffd0
David Dunn <qenopster@gmail.com>
2018-01-16 18:57:05 -0500

    bug fix

--- hw0/task_store.c
+++ hw0/task_store.c
@@ -25,7 +25,7 @@ void *store(char *parm, task *ptr){
     if (num_tasks == MAXSIZE - 1) return NULL;  // check maximum size exceeded
 
     // TODO replace this with actually copying data
-    task my_task = ptr;
+    task my_task = *ptr;
     task_entry new_entry = {.key = parm, .my_task = my_task};
     *(data+num_tasks) = new_entry;
     num_tasks += 1;
